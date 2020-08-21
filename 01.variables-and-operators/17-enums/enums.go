f5021deedc218519c43647f9ed68c6e98930adff
Filipe <filipe.bsouza@gmail.com>
2020-08-21 16:47:32 -0400

    Estudo de Go

---
+++ 01.variables-and-operators/17-enums/enums.go
@@ -0,0 +1,26 @@
+package main
+
+// Jeito de fazer na mão
+const (
+	Sunday    = 0
+	Monday    = 1
+	Tuesday   = 2
+	Wednesday = 3
+	Thursday  = 4
+	Friday    = 5
+	Saturday  = 6
+)
+
+// Jeito automático, atribui os números
+const (
+	Domingo = iota
+	Segunda
+	Terca
+	Quarta
+	Quinta
+	Sexta
+)
+
+func main() {
+	return
+}
