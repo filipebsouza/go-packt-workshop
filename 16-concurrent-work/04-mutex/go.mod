fc61730ac599a8ac18f6d4860c666b64bcfdf819
Filipe Bezerra de Souza <filipe.bsouza@gmail.com>
2022-02-06 17:56:51 -0400

    wip

---
+++ 16-concurrent-work/04-mutex/go.mod
@@ -0,0 +1,3 @@
+module 04-mutex
+
+go 1.17
---
+++ 16-concurrent-work/04-mutex/mutex.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+func sum(from, to int, wg *sync.WaitGroup, res *string, mtx *sync.Mutex) {
+	for i := from; i < to; i++ {
+		mtx.Lock() // lock for any other Goroutines not set value for the variable
+		*res = fmt.Sprintf("%s|%d|", *res, i)
+		mtx.Unlock()
+	}
+
+	wg.Done()
+	return
+}
+
+func main() {
+	s1 := ""
+	mtx := &sync.Mutex{}
+	wg := &sync.WaitGroup{}
+	wg.Add(4)
+	go sum(1, 25, wg, &s1, mtx)
+	go sum(26, 50, wg, &s1, mtx)
+	go sum(51, 75, wg, &s1, mtx)
+	go sum(76, 100, wg, &s1, mtx)
+	wg.Wait()
+	log.Println(s1)
+}
